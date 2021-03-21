33f8e337f9b04c01f8498ff3550260e357879d45
Rob Hofstetter <hofstetr@att.net>
2021-03-21 13:02:54 -0500

    Fixed an index out of bounds exception.

--- GraphCS/src/cloud/hofstetr/csgraph/model/ContentItem.java
+++ GraphCS/src/cloud/hofstetr/csgraph/model/ContentItem.java
@@ -111,7 +111,7 @@ public class ContentItem extends DefaultMutableTreeNode implements Comparable<Co
 		
 		// Create alphabetical grouping nodes so that a single pie chart will have a manageable number of slices
 		String alphabet = "abcdefghijklmnopqrstuvwxyz";
-		for (int i=0; i<alphabet.length()-1; i++) {
+		for (int i=0; i<alphabet.length(); i++) {
 			ContentItem ci = new ContentItem(alphabet.substring(i,i+1), "Folder", alphabet.substring(i,i+1), 0);
 			this.add(ci);
 		}
