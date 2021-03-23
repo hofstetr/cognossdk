ad33b317dc121e7677a47e4a31b5310207bedfef
Rob Hofstetter <hofstetr@att.net>
2021-03-22 23:02:09 -0500

    Fixed a bug with progress bar in GraphCS.

--- GraphCS/src/cloud/hofstetr/csgraph/view/ProgressDialog.java
+++ GraphCS/src/cloud/hofstetr/csgraph/view/ProgressDialog.java
@@ -1,6 +1,10 @@
 package cloud.hofstetr.csgraph.view;
 
+import java.awt.BorderLayout;
+
+import javax.swing.GroupLayout;
 import javax.swing.JDialog;
+import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 
 public class ProgressDialog extends JDialog {
@@ -8,8 +12,28 @@ public class ProgressDialog extends JDialog {
 	private JProgressBar Bar;
 	
 	public ProgressDialog() {
+		JPanel progressPanel = new JPanel();
+		GroupLayout groupLayout = new GroupLayout(progressPanel);
+		groupLayout.setAutoCreateGaps(true);
+		progressPanel.setLayout(groupLayout);
+		
 		Bar = new JProgressBar();
 		Bar.setValue(0);
+		Bar.setMaximum(100);
+		
+		groupLayout.setHorizontalGroup(groupLayout.createSequentialGroup()
+		        .addGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+		                .addComponent(Bar)));
+		
+		groupLayout.setVerticalGroup(groupLayout.createSequentialGroup()
+		        .addGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+		                .addComponent(Bar)));
+		
+		this.setTitle("Progress");
+		this.getContentPane().add(progressPanel, BorderLayout.CENTER);
+		this.pack();
+		this.setModal(true);
+		this.setVisible(true);
 	}
 	
 	public JProgressBar getBar() {
