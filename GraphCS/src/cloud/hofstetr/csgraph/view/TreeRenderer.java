16055ed6ddce01766a90af84fcfa5b4bd36dd2a6
Rob Hofstetter <hofstetr@att.net>
2021-03-29 14:39:15 -0500

    Changed icon folder structure to work inside JAR

--- GraphCS/src/cloud/hofstetr/csgraph/view/TreeRenderer.java
+++ GraphCS/src/cloud/hofstetr/csgraph/view/TreeRenderer.java
@@ -1,8 +1,6 @@
 package cloud.hofstetr.csgraph.view;
 
 import java.awt.Component;
-import java.awt.Toolkit;
-
 import javax.swing.ImageIcon;
 import javax.swing.JTree;
 import javax.swing.tree.DefaultTreeCellRenderer;
@@ -11,11 +9,11 @@ import cloud.hofstetr.csgraph.model.ContentItem;
 
 public class TreeRenderer extends DefaultTreeCellRenderer {
 	private static final long serialVersionUID = 8727700150613116574L;
-	private ImageIcon MyFolder = new ImageIcon(Toolkit.getDefaultToolkit().getImage("icon_myfolder.gif"));
-	private ImageIcon TeamFolder = new ImageIcon(Toolkit.getDefaultToolkit().getImage("icon_publicfolder.gif"));
-	private ImageIcon Folder = new ImageIcon(Toolkit.getDefaultToolkit().getImage("icon_folder.gif"));
-	private ImageIcon Model = new ImageIcon(Toolkit.getDefaultToolkit().getImage("icon_model.gif"));
-	private ImageIcon Report = new ImageIcon(Toolkit.getDefaultToolkit().getImage("icon_report.gif"));
+	private ImageIcon MyFolder = new ImageIcon(getClass().getResource("/images/icon_myfolder.gif"));
+	private ImageIcon TeamFolder = new ImageIcon(getClass().getResource("/images/icon_publicfolder.gif"));
+	private ImageIcon Folder = new ImageIcon(getClass().getResource("/images/icon_folder.gif"));
+	private ImageIcon Model = new ImageIcon(getClass().getResource("/images/icon_model.gif"));
+	private ImageIcon Report = new ImageIcon(getClass().getResource("/images/icon_report.gif"));
 	
 	public Component getTreeCellRendererComponent(
             JTree tree,
